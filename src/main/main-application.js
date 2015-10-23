562e4138418c8502ef814d5ab74bc39f058ecc8e
sabazusi <kouhei.live@gmail.com>
2015-10-23 22:02:06 +0900

    translate tabs to 4-space.

--- src/main/main-application.js
+++ src/main/main-application.js
@@ -7,21 +7,21 @@ import ipc from 'ipc'
 
 export default class MainApplication
 {
-	constructor() {
-		this.window = null;
-	}
+    constructor() {
+        this.window = null;
+    }
 
-	start() {
-		ipc.on('log', this._log);
-		ipc.on('authenticate-twitter', this._authentication.bind(this));
-		ipc.on('login-twitter', this._login.bind(this));
-		app.on('ready', this._onReady.bind(this));
-	}
+    start() {
+        ipc.on('log', this._log);
+        ipc.on('authenticate-twitter', this._authentication.bind(this));
+        ipc.on('login-twitter', this._login.bind(this));
+        app.on('ready', this._onReady.bind(this));
+    }
 
-	_onReady() {
-		this._loadCredential();
-		this.mainWindow = new MainWindow();
-	}
+    _onReady() {
+        this._loadCredential();
+        this.mainWindow = new MainWindow();
+    }
 
     _loadCredential() {
         let credentialFilePath = path.resolve(__dirname, '../../', 'resources', 'authentication', 'twitter-credential.json');
@@ -29,26 +29,26 @@ export default class MainApplication
         this._credential = {consumerKey: credential.consumerKey, consumerSecret: credential.consumerSecret, callback:credential.callback};
     }
 
-	_authentication() {
-		console.log('authentication start.');
-		let credential = this._credential;
-		let callback = this._login;
+    _authentication() {
+        console.log('authentication start.');
+        let credential = this._credential;
+        let callback = this._login;
 
-		setTimeout(function(){
-			this.authenticationWindow = new AuthenticationWindow();
-			this.authenticationWindow.on('get-access-token', callback);
-			this.authenticationWindow.show(credential);
-		}, 1000);
+        setTimeout(function(){
+            this.authenticationWindow = new AuthenticationWindow();
+            this.authenticationWindow.on('get-access-token', callback);
+            this.authenticationWindow.show(credential);
+        }, 1000);
 
-		this.mainWindow.send('login-succeeded');
-	}
+        this.mainWindow.send('login-succeeded');
+    }
 
-	_login(accessToken, accessTokenSecret) {
-		console.log(accessToken);
-		console.log(accessTokenSecret);
-	}
+    _login(accessToken, accessTokenSecret) {
+        console.log(accessToken);
+        console.log(accessTokenSecret);
+    }
 
-	_log(event, args) {
-		console.log("[Renderer]" + args);
-	}
+    _log(event, args) {
+        console.log("[Renderer]" + args);
+    }
 }
--- src/main/window/main-window.js
+++ src/main/window/main-window.js
@@ -3,27 +3,27 @@ import path from 'path'
 
 export default class MainWindow
 {
-	constructor() {
-		const windowProperty = 
-		{
-			width: 400,
-			height: 700,
-			frame: true,
-			resizable: false
-		};
+    constructor() {
+        const windowProperty = 
+        {
+            width: 400,
+            height: 700,
+            frame: true,
+            resizable: false
+        };
 
-		this._window = electronWindow.createWindow(windowProperty);
-		this.show();
-	}
+        this._window = electronWindow.createWindow(windowProperty);
+        this.show();
+    }
 
-	show() {
-		let renderFilePath = path.resolve(__dirname, '../../', 'renderer', 'main.html');
-		this._window.showUrl(renderFilePath, {}, function() {
-			console.log('window created.');
-		});
-	}
+    show() {
+        let renderFilePath = path.resolve(__dirname, '../../', 'renderer', 'main.html');
+        this._window.showUrl(renderFilePath, {}, function() {
+            console.log('window created.');
+        });
+    }
 
-	send(...args) {
-		this._window.webContents.send(...args);
-	}
+    send(...args) {
+        this._window.webContents.send(...args);
+    }
 }
