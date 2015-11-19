b5dcd53c957aaf687b0ef882d25282c618ddf24b
sabazusi <kouhei.live@gmail.com>
2015-11-19 20:09:34 +0900

    add render-preload method.

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from './components/root'
 import TweetList from './components/tweet-list'
+import PreLoad from './components/pre-load'
 
 export default class Renderer {
     render(stores) {
         ReactDOM.render(
             <Root stores={stores}/>, document.getElementById("postLoad")
+        );
+    }
+
+    renderPreload() {
+        ReactDOM.render(
+            <PreLoad/>, document.getElementById("preLoad")
         );
     }
 }
