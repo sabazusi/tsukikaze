253a53e78f49f757e5656475fd51bcfb70fc89ff
sabazusi <kouhei.live@gmail.com>
2015-11-22 23:23:20 +0900

    change button-status on click.

--- src/renderer/components/switcher.jsx
+++ src/renderer/components/switcher.jsx
@@ -2,17 +2,42 @@ import React from 'react'
 import { Button, ButtonToolbar, ButtonGroup } from 'react-bootstrap'
 
 export default class Switcher extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            active: [true, false, false]
+        }
+    }
+
     render() {
         return (
             <div className="switcher">
                 <ButtonToolbar>
                     <ButtonGroup>
-                        <Button bsSize="small">Timeline</Button>
-                        <Button bsSize="small">Mention</Button>
-                        <Button bsSize="small">DM</Button>
+                        <Button active={this.state.active[0]} onClick={this.showA.bind(this)} bsSize="small">Timeline</Button>
+                        <Button active={this.state.active[1]} onClick={this.showB.bind(this)} bsSize="small">Mention</Button>
+                        <Button active={this.state.active[2]} onClick={this.showC.bind(this)} bsSize="small">DM</Button>
                     </ButtonGroup>
                 </ButtonToolbar>
             </div>
         );
+    }
+
+    showA(e) {
+        this.setState({
+            active: [true, false, false]
+        });
+    }
+
+    showB(e) {
+        this.setState({
+            active: [false, true, false]
+        });
+    }
+
+    showC(e) {
+        this.setState({
+            active: [false, false, true]
+        });
     }
 }
