27d89e5f99f688b7c14b70b79ecdb1bc5f15168e
sabazusi <kouhei.live@gmail.com>
2015-11-24 16:10:07 +0900

    add editor component file.

---
+++ src/renderer/components/editor.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Editor extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="editor">
+            </div>
+        )
+    }
+}
