dde0b9bdebdfa6ba5f08e77bb65c2130434296f3
sabazusi <kouhei.live@gmail.com>
2015-12-01 20:47:08 +0900

    return tweet-body with link(not work...applying reactjs.)

--- src/renderer/components/tweet.jsx
+++ src/renderer/components/tweet.jsx
@@ -1,6 +1,36 @@
 import React from 'react'
+import twitterText from 'twitter-text'
 
 export default class Tweet extends React.Component {
+    getTweetBody() {
+        const origin = this.props.text;
+        const result = [];
+        let index = 0;
+        twitterText.extractEntitiesWithIndices(
+            origin, 
+            {extractUrlsWithoutProtocol:false}
+        ).forEach((entity) => {
+            if (index != entity.indices[0]) {
+                result.push(origin.substring(index, entity.indices[0]));
+            }
+            result.push(this.getTargetComponent(
+                    origin.substring(entity.indices[0], entity.indices[1]),
+                    entity
+                ));
+            index = entity.indices[1];
+        });
+        result.push(origin.substring(index, origin.length));
+        return result.join("");
+    }
+
+    getTargetComponent(targetText, entity) {
+        if (entity.url) {
+            return <a href={entity.url} dangerouslySetInnerHTML={targetText}/>;
+        } else {
+            return targetText;
+        }
+    }
+
     render() {
         return (
                 <div className="tweet">
@@ -8,7 +38,7 @@ export default class Tweet extends React.Component {
                         {this.props.name} @{this.props.screenName} <br />
                     </div>
                     <div className="tweet-body">
-                        {this.props.text}
+                        {this.getTweetBody()}
                     </div>
                 </div>);
     }
