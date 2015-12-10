da71c075b3802b4b3af8429c331f7778ccdc3f13
sabazusi <kouhei.live@gmail.com>
2015-12-10 20:13:14 +0900

    add un-fav action.

--- src/renderer/components/tweet-option.jsx
+++ src/renderer/components/tweet-option.jsx
@@ -37,7 +37,7 @@ export default class TweetOption extends React.Component {
         ViewDispatcher.dispatch({
             actionType: TweetOptionConstants.FAV_TWEET,
             tweetId: this.props.tweet.id_str,
-            isFav: true
+            isFav: !this.props.tweet.favorited
         });
     }
 
@@ -49,12 +49,13 @@ export default class TweetOption extends React.Component {
     }
 
     render() {
+        let favLabel = this.props.tweet.favorited ? "Unfavorite" : "Favorite";
         return (<div>
                     <ButtonToolbar>
                         <DropdownButton bsSize="small" title="" id="tweet-option-button">
                             <MenuItem eventKey="1" onClick={this.onMentionClicked.bind(this)}>Mention</MenuItem>
                             <MenuItem eventKey="2" onClick={this.onDMClicked.bind(this)}>DirectMessage</MenuItem>
-                            <MenuItem eventKey="3" onClick={this.onFavClicked.bind(this)}>Favorite</MenuItem>
+                            <MenuItem eventKey="3" onClick={this.onFavClicked.bind(this)}>{favLabel}</MenuItem>
                             <MenuItem eventKey="4" onClick={this.onRetweetClicked.bind(this)}>ReTweet</MenuItem>
                         </DropdownButton>
                     </ButtonToolbar>
