94d9096327588b5d15889f688cff077f9eea6025
sabazusi <kouhei.live@gmail.com>
2016-01-17 18:59:44 +0900

    mv ipc-constants to util directory.

--- src/renderer/constants/ipc-constants.js
+++
@@ -1,9 +0,0 @@
-class IpcConstants {
-    constructor() {
-        this.INITIALIZE_WITH_KEY = "initializeWithKey";
-        this.INITIALIZE_WITH_LOGIN = "initializeWithLogin";
-        this.UPDATE_LOGIN_KEYS = "updateLoginKeys";
-        this.UPDATE_WINDOW_SIZE = "updateWindowSize";
-    }
-}
-export default new IpcConstants();
