7dcf504c03511c643a2aedec0a7310f661a42954
sabazusi <kouhei.live@gmail.com>
2016-02-29 18:39:08 +0900

    add image modal on clicked

--- src/renderer/components/tweet/image.jsx
+++ src/renderer/components/tweet/image.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
+import Modal from 'react-modal';
 
 /**
  * TODO: ユーザー画像とツイート内画像の２つで使われてるので、分離する
  */
 export default class TweetImage extends React.Component {
-    onClickImage(e) {
+    constructor(...args) {
+        super(...args);
+        this.state = {
+            imageVisible: false
+        }
     }
+    onClickImage(e) {
+        this.setState({
+            imageVisible: true
+        });
+    }
+    onRequestClose() {
+        this.setState({
+            imageVisible: false
+        });
+    }
+
     render() {
         return (
-            <img
-                className="userimg"
-                src={this.props.imageUrl}
-                width={this.props.width}
-                height={this.props.height}
-                onClick={this.onClickImage}
-            />
+            <div>
+                <img
+                    className="userimg"
+                    src={this.props.imageUrl}
+                    width={this.props.width}
+                    height={this.props.height}
+                    onClick={this.onClickImage.bind(this)}
+                />
+                <Modal
+                    isOpen={this.state.imageVisible}
+                    closeTimeoutMS={50}
+                    onRequestClose={this.onRequestClose.bind(this)}
+                >
+                    <img src={this.props.imageUrl}/>
+                </Modal>
+            </div>
         );
     }
 }
