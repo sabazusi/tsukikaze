4a06b779e97bc14a84716ae389f96ceb7f166f39
sabazusi <kouhei.live@gmail.com>
2016-03-27 08:53:34 +0900

    move context-menu actions, and add clipboard

---
+++ src/renderer/actions/contextmenu-action.js
@@ -0,0 +1,6 @@
+export default class ContextMenuAction {
+    constructor() {
+        window.addEventListener('contextmenu', (e) => {
+        });
+    }
+}
--- src/renderer/main-starter.js
+++ src/renderer/main-starter.js
@@ -1,4 +1,5 @@
 import ActionDispatcher from './dispatcher/action-dispatcher';
+import ContextMenuAction from './actions/contextmenu-action';
 import DirectMessageStore from './stores/direct-message-store';
 import EditorAction from './actions/editor-action';
 import EditorStore from './stores/editor-store';
@@ -43,15 +44,7 @@ export default class MainApplicationStarter {
         new ExternalAction();
         new IpcAction();
         new TweetImageAction();
-
-        // initialize context menu.
-        let Menu = remote.require('menu');
-        let MenuItem = remote.require('menu-item');
-
-        let menu = new Menu();
-        window.addEventListener('contextmenu', (e) => {
-            // TODO: e.targetの内容を見てmenuItemを出し分ける
-        });
+        new ContextMenuAction();
 
         // start application.
         ReactDOM.render(
