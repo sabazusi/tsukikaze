da1d91fadef5b4b084f33dd6f1b658f7a1295e08
sabazusi <kouhei.live@gmail.com>
2016-03-29 15:46:42 +0900

    add context-menu template.

--- src/renderer/actions/contextmenu-action.js
+++ src/renderer/actions/contextmenu-action.js
@@ -1,6 +1,22 @@
+import remote from 'remote';
+
 export default class ContextMenuAction {
     constructor() {
+        this.Menu = remote.require('menu');
+        this.contextMenu = this.Menu.buildFromTemplate([
+            {label: 'todo: implement'}
+        ]);
         window.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            switch(e) {
+              default:
+                  this.getContextMenu();
+                  break;
+            }
         });
+    }
+
+    getContextMenu() {
+        this.contextMenu.popup(remote.getCurrentWindow());
     }
 }
