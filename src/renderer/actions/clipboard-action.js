3a304290b1124d59ce5705c99811887580de4e24
sabazusi <kouhei.live@gmail.com>
2016-04-02 10:56:16 +0900

    add clipboard constatns/

--- src/renderer/actions/clipboard-action.js
+++ src/renderer/actions/clipboard-action.js
@@ -1,5 +1,15 @@
+import ActionDispatcher from '../dispatcher/action-dispatcher';
+import ContextMenuConstants from '../constants/contextmenu-constants';
+
 export default class ClipboardAction {
     constructor() {
-
+        ActionDispatcher.register((action) => {
+            switch(action.actionType) {
+                case ContextMenuConstants.COPY_TEXT:
+                    break;
+                default:
+                    break;
+            }
+        });
     }
 }
